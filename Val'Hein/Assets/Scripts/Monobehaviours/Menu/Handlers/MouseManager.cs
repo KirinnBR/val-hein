98e053b1dae6fb017361bf3f474b7aa31f517164
KirinnBR <lucaspiresfernandes@gmail.com>
2019-11-04 11:21:04 -0300

    Added combo system and adjusting mouse manager and player combat

--- Val'Hein/Assets/Scripts/Monobehaviours/Menu/Handlers/MouseManager.cs
+++ Val'Hein/Assets/Scripts/Monobehaviours/Menu/Handlers/MouseManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseManager : MonoBehaviour
+public class MouseManager : Singleton<MouseManager>
 {
 	public bool IsMouseLocked { get; private set; }
 
--- Val'Hein/Assets/Scripts/Monobehaviours/Player/PlayerCombat.cs
+++ Val'Hein/Assets/Scripts/Monobehaviours/Player/PlayerCombat.cs
@@ -9,14 +9,23 @@ public class PlayerCombat : MonoBehaviour, IDamageable
 
 	[Header("Combat Settings")]
 
+	[Tooltip("The stats of the player.")]
 	[SerializeField]
 	private Stats stats;
+	[Tooltip("The armor that the player uses")]
 	[SerializeField]
 	private ArmorStatsIncreaser armor;
+	[Tooltip("The delay, in seconds, it takes to attack again.")]
 	[SerializeField]
 	private float timeBetweenAttacks = 0.5f;
 	[SerializeField]
+	[Tooltip("The layer to search for enemies to combat.")]
 	private LayerMask combatLayer;
+	[Tooltip("The time, in seconds, to validate the combo.")]
+	[SerializeField]
+	private float comboValidationSeconds = 1f;
+
+	private int CurrentAttackCombo { get; set; } = 0;
 
 	#endregion
 
@@ -27,7 +36,7 @@ public class PlayerCombat : MonoBehaviour, IDamageable
 	[SerializeField]
 	private MouseButtonCode buttonToAttack = MouseButtonCode.LeftButton;
 
-	private bool lightAttackInput;
+	private bool attackInput;
 
 	#endregion
 
@@ -35,19 +44,20 @@ public class PlayerCombat : MonoBehaviour, IDamageable
 
 	[Header("Motion Calibration Settings")]
 
-	[SerializeField]
-	private Vector3 hitBoxOffset = new Vector3(0f, 1f, 0.75f);
+	[Tooltip("The size of the hitbox.")]
 	[SerializeField]
 	private Vector3 hitBoxSize = Vector3.one * 0.5f;
+	[Tooltip("The position offset that calibrates the hitbox.")]
 	[SerializeField]
-	private float hitDistance = 1f;
+	private Vector3 hitBoxOffset = new Vector3(0f, 1f, 0.75f);
 
 	#endregion
 
 	public float CurrentHealth { get; private set; }
 
-	private bool canAttack = false;
+	private bool CanAttack { get; set; } = true;
 	private float currentCooldown = 0;
+	private Coroutine attackCoroutine;
 	private Animator anim;
 	// Start is called before the first frame update
 	void Start()
@@ -59,33 +69,33 @@ public class PlayerCombat : MonoBehaviour, IDamageable
     // Update is called once per frame
     void Update()
     {
-		CalculateCooldown();
+		UpdateCooldown();
 		CalculateInput();
 		TryCombat();
 	}
 
-	private void CalculateCooldown()
+	private void UpdateCooldown()
 	{
-		if (!canAttack)
+		if (!CanAttack)
 		{
 			currentCooldown -= Time.deltaTime;
 			if (currentCooldown <= 0)
 			{
-				canAttack = true;
+				CanAttack = true;
 			}
 		}
 	}
 
 	private void CalculateInput()
 	{
-		lightAttackInput = Input.GetMouseButtonDown((int)buttonToAttack);
+		attackInput = Input.GetMouseButtonDown((int)buttonToAttack);
 	}
 
 	private void TryCombat()
 	{
-		if (canAttack)
+		if (CanAttack)
 		{
-			if (lightAttackInput)
+			if (attackInput)
 			{
 				Attack();
 				ActivateCooldown();
@@ -95,20 +105,49 @@ public class PlayerCombat : MonoBehaviour, IDamageable
 
 	private void Attack()
 	{
-		anim.SetTrigger("Light Attack");
-		if (Physics.BoxCast(transform.position + hitBoxOffset, hitBoxSize / 2, transform.forward, out RaycastHit hit, Quaternion.LookRotation(transform.forward), hitDistance, combatLayer, QueryTriggerInteraction.Ignore))
+		ProccessAttackAnimation();
+		var objects = Physics.OverlapBox(transform.position + hitBoxOffset + transform.forward, hitBoxSize / 2, Quaternion.LookRotation(transform.forward), combatLayer, QueryTriggerInteraction.Ignore);
+		bool resetedCombo = false;
+		foreach (var obj in objects)
 		{
-			if (hit.transform.TryGetComponent(out IDamageable dmg))
+			if (obj.TryGetComponent(out IDamageable dmg))
 			{
-				//Do damage and instantiate blood and shit.
+				//Stops the last combo validation and starts a new one.
+				if (!resetedCombo)
+				{
+					if (attackCoroutine != null)
+						StopCoroutine(attackCoroutine);
+					attackCoroutine = StartCoroutine(AttackComboValidation());
+					resetedCombo = true;
+				}
+
+				//Do damage to damageable objects inside the hitbox.
 				dmg.TakeDamage(stats.baseStrength);
 			}
 		}
 	}
 
+	private void ProccessAttackAnimation()
+	{
+		Debug.Log(CurrentAttackCombo);
+		switch (CurrentAttackCombo)
+		{
+			case (int)AttackComboType.LightAttack:
+				anim.SetTrigger("Light Attack");
+				break;
+			case (int)AttackComboType.NormalAttack:
+				anim.SetTrigger("Normal Attack");
+				break;
+			case (int)AttackComboType.HeavyAttack:
+				anim.SetTrigger("Heavy Attack");
+				CurrentAttackCombo = -1;
+				break;
+		}
+	}
+
 	private void ActivateCooldown()
 	{
-		canAttack = false;
+		CanAttack = false;
 		currentCooldown = timeBetweenAttacks;
 	}
 
@@ -126,10 +165,22 @@ public class PlayerCombat : MonoBehaviour, IDamageable
 		//Call for endgame.
 	}
 
+	private IEnumerator AttackComboValidation()
+	{
+		CurrentAttackCombo++;
+		yield return new WaitForSeconds(comboValidationSeconds);
+		CurrentAttackCombo = 0;
+	}
+
 	private void OnDrawGizmosSelected()
 	{
 		Gizmos.color = Color.red;
-		Gizmos.DrawWireCube(transform.position + hitBoxOffset + transform.forward * hitDistance, hitBoxSize);
+		Gizmos.DrawWireCube(transform.position + hitBoxOffset + transform.forward, hitBoxSize);
 	}
 
+}
+
+public enum AttackComboType : int
+{
+	LightAttack = 0, NormalAttack = 1, HeavyAttack = 3
 }
